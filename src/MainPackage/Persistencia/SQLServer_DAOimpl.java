c1f811aabdb2ba272d4806539fc78f2874cd50b4
galloafranco <galloafranco@gmail.com>
2018-07-23 02:00:27 -0300

    Creamos clase SQLServer_DAOimpl que implementa PedidoDAO, aun no tienen  soporte los metodos

---
+++ src/MainPackage/Persistencia/SQLServer_DAOimpl.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MainPackage.Persistencia;
+
+import MainPackage.Logica.Pedido;
+
+/**
+ *
+ * @author Franco
+ */
+public class SQLServer_DAOimpl implements PedidoDAO {
+
+    @Override
+    public void addPedido(Pedido pedido) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public StringBuilder mostrarTodo() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void borrarTodo() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
