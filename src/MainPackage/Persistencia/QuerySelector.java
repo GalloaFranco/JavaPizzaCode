ad3273cd7093b0e30542e0f5d21a616b1ea80e4d
galloafranco <galloafranco@gmail.com>
2018-08-01 22:28:19 -0300

    Quitamos comentarios de main y quitamos final en QuerySelector relacionados a la tabla Comidas la cual no va a tener contacto con la aplicacion

--- src/MainPackage/Persistencia/QuerySelector.java
+++ src/MainPackage/Persistencia/QuerySelector.java
@@ -13,20 +13,14 @@ public class QuerySelector {//ESTA CLASE CONSTA DE TODAS LAS QUERYS QUE SE UTILI
    
     //CAMPOS CONSTANTES DE LA CLASE QUE CONTIENEN 
     //LAS SENTENCIAS SQL A UTILIZAR POR NUESTRA IMPL DAO
-    private static final String SELECT_COMIDAS = "SELECT * FROM COMIDAS";
+
     private static final String SELECT_PEDIDOS = "SELECT * FROM PEDIDOS";
     private static final String SELECT_DETALLES = "SELECT * FROM DETALLE_PEDIDOS";
     
-    private static final String INSERT_COMIDAS = "INSERT INTO COMIDAS (ID_COMIDA,PRECIO,DESCRIPCION,MEDIDA) VALUES (?,?,?,?)";
+
     private static final String INSERT_PEDIDOS = "INSERT INTO PEDIDOS (NRO_PEDIDO,FECHA) VALUES (?,?)";
     private static final String INSERT_DETALLE = "INSERT INTO DETALLE_PEDIDOS (NRO_PEDIDO,ID_COMIDA) VALUES (?,?)";
     
-    /*
-    ****************************************************************************
-    */
-    public static String getSELECT_COMIDAS() {
-        return SELECT_COMIDAS;
-    }
     /*
     ****************************************************************************
     */
@@ -38,12 +32,6 @@ public class QuerySelector {//ESTA CLASE CONSTA DE TODAS LAS QUERYS QUE SE UTILI
     */
     public static String getSELECT_DETALLES() {
         return SELECT_DETALLES;
-    }
-    /*
-    ****************************************************************************
-    */
-    public static String getINSERT_COMIDAS() {
-        return INSERT_COMIDAS;
     }
     /*
     ****************************************************************************
